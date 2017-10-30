ce797cd15b96bca63bec3947f863815763974516
Pavel Senin <seninp@gmail.com>
2017-10-30 20:43:12 +0100

    pybind continues #2

--- setup.py
+++ setup.py
@@ -66,17 +66,7 @@ setup(
     description="SAX implementation for Python",
     long_description=open('README.rst').read(),
 
-    packages=setuptools.find_packages(),
-
-    install_requires=[],
-
-    classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-    ],
+    ext_modules=[CMakeExtension('saxpy')],
+    cmdclass=dict(build_ext=CMakeBuild),
+    zip_safe=False,
 )
