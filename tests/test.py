8d5508db0d87def0bddc9445eea28e08795bd4a6
Pavel Senin <seninp@gmail.com>
2017-11-01 18:23:20 +0100

    adding a first real test

--- tests/test.py
+++ tests/test.py
@@ -1,4 +1,13 @@
-# Sample Test passing with nose and pytest
+import numpy as np
+import pytest
+import saxpy as sp
 
-def test_pass():
-    assert True, "dummy sample test"
+def test_znorm():
+
+    ts = [-1., -2., -1., 0., 2., 1., 1., 0.]
+    z_thrsh = 0.001
+
+    x_scaled = map(lambda i: i/100.0, ts)
+
+    assert pytest.approx(1.0, 0.000001) == \
+                        np.std(sp.znorm(x_scaled, z_thrsh), axis=0, ddof=1)
