830371fb6c37390b540284a4d6bdf912ef9e0ce8
Pavel Senin <seninp@gmail.com>
2017-11-09 21:20:51 +0100

    typo fix

--- src/subseries.cpp
+++ src/subseries.cpp
@@ -2,7 +2,7 @@
 #include <numeric>
 #include <vector>
 
-void init_znorm(py::module &m) {
+void init_subseries(py::module &m) {
     m.def("subseries", [](std::vector<double> ts, int start, int end) {
   std::vector<double>::const_iterator first = ts.begin() + start;
   std::vector<double>::const_iterator last = ts.begin() + end;
