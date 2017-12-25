1123414c3847984bdbf4b9b8454b20f52734f28d
Pavel Senin <seninp@gmail.com>
2017-12-25 05:57:10 +0100

    distance tests

--- tests/test_distance.py
+++ tests/test_distance.py
@@ -12,3 +12,17 @@ def test_euclidean():
     a = np.array([0.5, 0.8, 0.9])
     b = np.array([-0.15, 0.38, 0.92])
     assert pytest.approx(0.7741447, 0.0000001) == distance.euclidean(a, b)
+
+
+def test_early_abandoned():
+    """Test euclidean distance."""
+    assert pytest.approx(np.sqrt(2), 0.0000001) ==\
+        distance.early_abandoned_dist(np.array([1., 1.]),
+                                      np.array([2., 2.]), np.inf)
+
+    a = np.array([0.5, 0.8, 0.9])
+    b = np.array([-0.15, 0.38, 0.92])
+    assert pytest.approx(0.7741447, 0.0000001) ==\
+        distance.early_abandoned_dist(a, b, np.inf)
+
+    assert 1 == np.isnan(distance.early_abandoned_dist(a, b, 0.1))
