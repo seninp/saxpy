a43b5d485f3a592f9781255d15b5ce1cc19ead31
Pavel Senin <seninp@gmail.com>
2017-12-31 18:01:39 +0100

    fixin da tezt

--- tests/test_discord_bruteforce.py
+++ tests/test_discord_bruteforce.py
@@ -1,10 +1,10 @@
 """Testing brute force discord search implementation."""
 import numpy as np
-from saxpy.discord import find_best_discord_brute_force
+from saxpy.discord import find_discords_brute_force
 
 
 def test_brute_force():
     """Test brute force discord discovery."""
     dd = np.genfromtxt("data/ecg0606_1.csv", delimiter=',')
-    discord = find_best_discord_brute_force(dd[0:400], 100)
-    assert 173 == discord[0]
+    discords = find_discords_brute_force(dd[0:400], 100, 2)
+    assert 173 == discords[0][0]
