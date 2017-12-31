e1b7dccf3b03a489f23e0879866f2c61a90c700c
Pavel Senin <seninp@gmail.com>
2017-12-31 18:05:10 +0100

    adding some to tests

--- tests/test_discord_bruteforce.py
+++ tests/test_discord_bruteforce.py
@@ -6,5 +6,6 @@ from saxpy.discord import find_discords_brute_force
 def test_brute_force():
     """Test brute force discord discovery."""
     dd = np.genfromtxt("data/ecg0606_1.csv", delimiter=',')
-    discords = find_discords_brute_force(dd[0:400], 100, 2)
+    discords = find_discords_brute_force(dd[0:400], 100, 4)
+    assert 3 == len(discords)
     assert 173 == discords[0][0]
