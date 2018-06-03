e112d1929f3145a55ab6e74d903ace1de9065424
Pavel Senin <seninp@gmail.com>
2018-06-03 10:53:38 +0200

    removing duplicates

--- tests/test_cuts0.py
+++
@@ -1,8 +0,0 @@
-"""Testing PAA implementation."""
-from saxpy import alphabet
-
-
-def test_sizing():
-    """Test alphabet sizes."""
-    for s in range(2, 20):
-        assert len(alphabet.cuts_for_asize(s)) == s
