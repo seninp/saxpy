550a701dc7bb679fc91c63579e8a6367ac9eda96
Ameya Daigavane <ameya.s.daigavane@jpl.nasa.gov>
2019-05-28 16:20:54 -0700

    Renamed z_threshold to znorm_threshold for consistency.

--- saxpy/sax.py
+++ saxpy/sax.py
@@ -4,7 +4,7 @@ from saxpy.strfunc import idx2letter
 from saxpy.znorm import znorm
 from saxpy.paa import paa
 from saxpy.alphabet import cuts_for_asize
-
+import numpy as np
 
 def ts_to_string(series, cuts):
     """A straightforward num-to-string conversion."""
@@ -37,26 +37,31 @@ def is_mindist_zero(a, b):
     return 1
 
 
-def sax_by_chunking(series, paa_size, alphabet_size=3, z_threshold=0.01):
+def sax_by_chunking(series, paa_size, alphabet_size=3, znorm_threshold=0.01):
     """Simple chunking conversion implementation."""
-    paa_rep = paa(znorm(series, z_threshold), paa_size)
+    paa_rep = paa(znorm(series, znorm_threshold), paa_size)
     cuts = cuts_for_asize(alphabet_size)
     return ts_to_string(paa_rep, cuts)
 
 
 def sax_via_window(series, win_size, paa_size, alphabet_size=3,
-                   nr_strategy='exact', z_threshold=0.01):
+                   nr_strategy='exact', znorm_threshold=0.01):
     """Simple via window conversion implementation."""
+
+    # Convert to numpy array.
+    series = np.array(series)
+
     cuts = cuts_for_asize(alphabet_size)
     sax = defaultdict(list)
 
     prev_word = ''
 
-    for i in range(0, len(series) - win_size):
+    # Sliding window across time dimension.
+    for i in range(0, series.shape[0] - win_size):
+        
+        sub_section = series[i: i + win_size]
 
-        sub_section = series[i:(i+win_size)]
-
-        zn = znorm(sub_section, z_threshold)
+        zn = znorm(sub_section, znorm_threshold)
 
         paa_rep = paa(zn, paa_size)
 
