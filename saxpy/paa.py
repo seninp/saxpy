d557911280f23513b8652e89186228cedd489ee9
Ameya Daigavane <ameya.s.daigavane@jpl.nasa.gov>
2019-05-28 17:26:30 -0700

    Removed 'trivial case' which was causing an error for multidimensional series.

--- saxpy/paa.py
+++ saxpy/paa.py
@@ -9,24 +9,21 @@ def paa(series, paa_segment_size):
     series = np.array(series)
     series_len = series.shape[0]
 
-    # check for the trivial case
-    if series_len == paa_segment_size:
-        return np.copy(series)
+    res = np.zeros(paa_segment_size)
+
+    # Check if we can evenly divide
+    if series_len % paa_segment_size == 0:
+        inc = series_len // paa_segment_size
+        for i in range(0, series_len):
+            idx = i // inc
+            np.add.at(res, idx, l2norm(series[i]))
+            # res[idx] = res[idx] + ||series[i]||
+        return res / inc
+    # Process otherwise
     else:
-        res = np.zeros(paa_segment_size)
-        # check when we are even
-        if series_len % paa_segment_size == 0:
-            inc = series_len // paa_segment_size
-            for i in range(0, series_len):
-                idx = i // inc
-                np.add.at(res, idx, l2norm(series[i]))
-                # res[idx] = res[idx] + series[i]
-            return res / inc
-        # and process when we are odd
-        else:
-            for i in range(0, paa_segment_size * series_len):
-                idx = i // series_len
-                pos = i // paa_segment_size
-                np.add.at(res, idx, l2norm(series[pos]))
-                # res[idx] = res[idx] + series[pos]
-            return res / series_len
+        for i in range(0, paa_segment_size * series_len):
+            idx = i // series_len
+            pos = i // paa_segment_size
+            np.add.at(res, idx, l2norm(series[pos]))
+            # res[idx] = res[idx] + ||series[pos]||
+        return res / series_len
