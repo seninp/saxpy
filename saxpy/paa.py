885ea11ca5646d30c4f28ca9381801af58915f25
Ameya Daigavane <ameya.s.daigavane@jpl.nasa.gov>
2019-05-28 22:34:33 -0700

    PAA extended to handle SAX-REPEAT.

--- saxpy/paa.py
+++ saxpy/paa.py
@@ -4,26 +4,35 @@ import numpy as np
 from znorm import l2norm
 
 
-def paa(series, paa_segment_size):
+def paa(series, paa_segment_size, sax_type):
     """PAA implementation."""
     series = np.array(series)
     series_len = series.shape[0]
 
-    res = np.zeros(paa_segment_size)
-
-    # Check if we can evenly divide
-    if series_len % paa_segment_size == 0:
-        inc = series_len // paa_segment_size
-        for i in range(0, series_len):
-            idx = i // inc
-            np.add.at(res, idx, np.mean(series[i]))
-            # res[idx] = res[idx] + ||series[i]||
-        return res / inc
-    # Process otherwise
+    if sax_type == 'repeat':
+        num_dims = series.shape[1]
     else:
-        for i in range(0, paa_segment_size * series_len):
-            idx = i // series_len
-            pos = i // paa_segment_size
-            np.add.at(res, idx, np.mean(series[pos]))
-            # res[idx] = res[idx] + ||series[pos]||
-        return res / series_len
+        num_dims = 1
+
+    res = np.zeros((num_dims, paa_segment_size))
+
+    for dim in range(num_dims):
+        # Check if we can evenly divide
+        if series_len % paa_segment_size == 0:
+            inc = series_len // paa_segment_size
+            for i in range(0, series_len):
+                idx = i // inc
+                np.add.at(res[dim], idx, np.mean(series[i]))
+            res[dim] /= inc
+        # Process otherwise
+        else:
+            for i in range(0, paa_segment_size * series_len):
+                idx = i // series_len
+                pos = i // paa_segment_size
+                np.add.at(res, idx, np.mean(series[pos]))
+            res[dim] /= series_len
+
+    if sax_type == 'repeat':
+        return res
+    else:
+        return res.flatten()
