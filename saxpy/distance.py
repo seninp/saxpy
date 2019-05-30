f578f878b2815963527d45fcfdea04cfd171cf68
Ameya Daigavane <ameya.s.daigavane@jpl.nasa.gov>
2019-05-30 10:56:39 -0700

    Removing early-abandoning temporarily.

--- saxpy/distance.py
+++ saxpy/distance.py
@@ -4,15 +4,15 @@ import numpy as np
 
 def euclidean(a, b):
     """Compute a Euclidean distance value."""
-    return np.sqrt(np.sum((a-b)**2))
+    return np.sqrt(np.sum(np.square(a-b)))
 
 
-def early_abandoned_dist(a, b, upper_limit):
+def early_abandoned_euclidean(a, b, upper_limit):
     """Compute a Euclidean distance value in early abandoning fashion."""
-    lim = upper_limit * upper_limit
+    lim = np.square(upper_limit)
     res = 0.
     for i in range(0, len(a)):
-        res += np.dot((a[i]-b[i]), (a[i]-b[i]))
-        if res > lim:
-            return np.nan
+        res += np.sum(np.square((a[i]-b[i])))
+        # if res > lim:
+        #     return np.nan
     return np.sqrt(res)
