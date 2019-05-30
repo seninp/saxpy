85b74f202369611a0d849f4dddac570d25611e5c
Ameya Daigavane <ameya.s.daigavane@jpl.nasa.gov>
2019-05-29 22:57:05 -0700

    Update early abandoning distance for multi-dimensional sequences, fixing previous.

--- saxpy/distance.py
+++ saxpy/distance.py
@@ -12,7 +12,7 @@ def early_abandoned_dist(a, b, upper_limit):
     lim = upper_limit * upper_limit
     res = 0.
     for i in range(0, len(a)):
-        res += (a[i]-b[i])*(a[i]-b[i])
-        if np.any(res > lim):
+        res += np.dot((a[i]-b[i]), (a[i]-b[i]))
+        if res > lim:
             return np.nan
     return np.sqrt(res)
