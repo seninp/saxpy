5e6523bde5420972ee8f24320d0b049adaf377e7
Ameya Daigavane <ameya.s.daigavane@jpl.nasa.gov>
2019-05-30 10:59:39 -0700

    Revert "Removing early-abandoning temporarily."
    This reverts commit f578f878b2815963527d45fcfdea04cfd171cf68.

--- saxpy/distance.py
+++ saxpy/distance.py
@@ -4,15 +4,15 @@ import numpy as np
 
 def euclidean(a, b):
     """Compute a Euclidean distance value."""
-    return np.sqrt(np.sum(np.square(a-b)))
+    return np.sqrt(np.sum((a-b)**2))
 
 
-def early_abandoned_euclidean(a, b, upper_limit):
+def early_abandoned_dist(a, b, upper_limit):
     """Compute a Euclidean distance value in early abandoning fashion."""
-    lim = np.square(upper_limit)
+    lim = upper_limit * upper_limit
     res = 0.
     for i in range(0, len(a)):
-        res += np.sum(np.square((a[i]-b[i])))
-        # if res > lim:
-        #     return np.nan
+        res += np.dot((a[i]-b[i]), (a[i]-b[i]))
+        if res > lim:
+            return np.nan
     return np.sqrt(res)
