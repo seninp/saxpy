6eed5b6b32d3d129c4451ad19590774889337a2e
Ameya Daigavane <ameya.s.daigavane@jpl.nasa.gov>
2019-05-31 11:24:42 -0700

    Updated test with renamed function.

--- tests/test_distance.py
+++ tests/test_distance.py
@@ -17,12 +17,12 @@ def test_euclidean():
 def test_early_abandoned():
     """Test euclidean distance."""
     assert pytest.approx(np.sqrt(2), 0.0000001) ==\
-        distance.early_abandoned_dist(np.array([1., 1.]),
+        distance.early_abandoned_euclidean(np.array([1., 1.]),
                                       np.array([2., 2.]), np.inf)
 
     a = np.array([0.5, 0.8, 0.9])
     b = np.array([-0.15, 0.38, 0.92])
     assert pytest.approx(0.7741447, 0.0000001) ==\
-        distance.early_abandoned_dist(a, b, np.inf)
+        distance.early_abandoned_euclidean(a, b, np.inf)
 
-    assert 1 == np.isnan(distance.early_abandoned_dist(a, b, 0.1))
+    assert 1 == np.isnan(distance.early_abandoned_euclidean(a, b, 0.1))
