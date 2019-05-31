Fixed off-by-one error in marking as visited.

saxpy/discord.py
@@ -5,8 +5,32 @@ from saxpy.distance import early_abandoned_euclidean
 from saxpy.znorm import znorm
 
 
-def find_best_discord_brute_force(series, win_size, global_registry,
-                                  z_threshold=0.01):
+def find_discords_brute_force(series, win_size, num_discords=2, znorm_threshold=0.01):
+    """Early-abandoned distance-based discord discovery."""
+    discords = list()
+
+    globalRegistry = VisitRegistry(len(series) - win_size + 1)
+
+    while len(discords) < num_discords:
+
+        bestDiscord = find_best_discord_brute_force(series, win_size,
+                                                    globalRegistry,
+                                                    znorm_threshold)
+
+        if -1 == bestDiscord[0]:
+            break
+
+        discords.append(bestDiscord)
+
+        mark_start = max(0, bestDiscord[0] - win_size + 1)
+        mark_end = bestDiscord[0] + win_size
+
+        globalRegistry.mark_visited_range(mark_start, mark_end)
+
+    return discords
+
+
+def find_best_discord_brute_force(series, win_size, global_registry, znorm_threshold=0.01):
     """Early-abandoned distance-based discord discovery."""
     best_so_far_distance = -1.0
     best_so_far_index = -1
@@ -19,8 +43,7 @@ def find_best_discord_brute_force(series, win_size, global_registry,
 
         outerRegistry.mark_visited(outer_idx)
 
-        candidate_seq = znorm(series[outer_idx:(outer_idx+win_size)],
-                              z_threshold)
+        candidate_seq = znorm(series[outer_idx:(outer_idx+win_size)], znorm_threshold)
 
         nnDistance = np.inf
         innerRegistry = VisitRegistry(len(series) - win_size + 1)
@@ -32,10 +55,9 @@ def find_best_discord_brute_force(series, win_size, global_registry,
 
             if abs(inner_idx - outer_idx) >= win_size:
 
-                curr_seq = znorm(series[inner_idx:(inner_idx+win_size)],
-                                 z_threshold)
-                dist = early_abandoned_euclidean(candidate_seq,
-                                            curr_seq, nnDistance)
+                curr_seq = znorm(series[inner_idx:(inner_idx+win_size)], znorm_threshold)
+
+                dist = early_abandoned_euclidean(candidate_seq, curr_seq, nnDistance)
 
                 if (~np.isnan(dist)) and (dist < nnDistance):
                     nnDistance = dist
@@ -49,34 +71,3 @@ def find_best_discord_brute_force(series, win_size, global_registry,
         outer_idx = outerRegistry.get_next_unvisited()
 
     return (best_so_far_index, best_so_far_distance)
-
-
-def find_discords_brute_force(series, win_size, num_discords=2,
-                              z_threshold=0.01):
-    """Early-abandoned distance-based discord discovery."""
-    discords = list()
-
-    globalRegistry = VisitRegistry(len(series) - win_size + 1)
-
-    while len(discords) < num_discords:
-
-        bestDiscord = find_best_discord_brute_force(series, win_size,
-                                                    globalRegistry,
-                                                    z_threshold)
-
-        if -1 == bestDiscord[0]:
-            break
-
-        discords.append(bestDiscord)
-
-        mark_start = bestDiscord[0] - win_size
-        if 0 > mark_start:
-            mark_start = 0
-
-        mark_end = bestDiscord[0] + win_size
-        '''if len(series) < mark_end:
-            mark_end = len(series)'''
-
-        globalRegistry.mark_visited_range(mark_start, mark_end)
-
-    return discords